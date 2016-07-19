91a6c43234898923917c2f9e53704551a4aa32e1
Vance Miller <vance@cs.utexas.edu>
2016-07-19 15:25:53 -0500

    Add comments

--- src/MultilevelCache.cpp
+++ src/MultilevelCache.cpp
@@ -12,16 +12,6 @@
 #include <math.h>
 #include <algorithm>
 
-/**
- * Constructs a multilevel cache.
- *
- * First level cache must be specified in the first entry of vector, second
- * in second entry and so on.
- *
- * @param capacities_B Cache capacities for each level of the cache, in bytes.
- * @param associativities Cache associativities for each level of the cache.
- * @param line_size_B The number of bytes each cache line will hold, defaults to 64.
- */
 MultilevelCache::MultilevelCache(const std::vector<uint32_t>& capacities_B,
     const std::vector<uint32_t>& associativities, const uint32_t line_size_B) :
     n_levels(capacities_B.size()), line_size_B(line_size_B) {
@@ -49,21 +39,6 @@ MultilevelCache::~MultilevelCache() {
   }
 }
 
-/**
- * Searches the cache for the CacheLine containing the requested address.
- *
- * Algorithm:
- * 1.  If the line is in the first level cache, fetch it and return.
- * 2.  Else, if the first level cache is full, evict a line.
- * 3.  Search the next level cache for the line. If we find it, remove it.
- * 4.  If we previously evicted a line and the cache is full, evict a line.
- * 4.1.Insert the previously evicted line into the cache.
- * 5.  Repeat 3-4.1 until the end of the cache hierarchy.
- * 6.  If we evicted a line from the LLC, destroy it.
- * 7.  If we have not found the requested cache line, create it (ie. fetch from main memory).
- * 8.  Insert the requested cache line into the L1.
- *
- */
 std::vector<CacheLine*>& MultilevelCache::Access(const ADDRESS address,
     const uint8_t n_bytes) {
   return SplitAccess(address, n_bytes);
@@ -95,88 +70,6 @@ std::vector<CacheLine*>& MultilevelCache::SplitAccess(const ADDRESS address,
         bytes_remaining < line_size_B ? bytes_remaining : line_size_B;
   } while (bytes_remaining > 0);
   return *accessed_lines;
-}
-
-/**
- * Searches the cache for the CacheLine containing the requested address.
- *
- * Algorithm:
- * 1.  If the line is in the first level cache, read it and return.
- * 2.  Else, if the first level cache is full, evict a line.
- * 3.  Search the next level cache for the line.
- * 4.  If we previously evicted a line and the cache is full, evict a line.
- * 4.1.Insert the previously evicted line into the cache.
- * 5.  Repeat 3-4.1 until the end of the cache hierarchy.
- * 6.  If we evicted a line from the LLC, destroy it and all references to it in other levels.
- * 7.  If we have not found the requested cache line, create it (ie. fetch from main memory).
- * 8.  Insert the requested cache line into the L1.
- *
- */
-CacheLine& MultilevelCache::SearchExclusive(const ADDRESS address,
-    const uint8_t size_B) {
-  CacheLine* requested = NULL;
-
-  std::vector<Cache*>::iterator cache = caches.begin();
-
-  // First, search the L1 for the line
-  requested = (*cache)->GetLine(address);
-
-  CacheLine* evicted = NULL;
-  // If the line was not found in L1, evict a line if the cache is full
-  if (requested == NULL) {
-    // Evict returns NULL if there is space available.
-    evicted = (*cache)->EvictLRU(address);
-  }
-
-  cache++;
-
-  // If we have evicted something from the previous level, insert it into the current level
-  // while continuing to search for the requested line.
-  while (cache < caches.end() && requested == NULL && evicted != NULL) {
-    // Search for the requested line
-    if ((*cache)->Contains(address)) {
-      requested = (*cache)->GetLine(address);
-      // Remove the line from the cache and move it to L1 (exclusive cache).
-      (*cache)->RemoveLine(address);
-    }
-    // Evict a line if the cache is full to make room for the evicted line
-    CacheLine* const tmp = (*cache)->EvictLRU(evicted->address);
-    // Insert the previously evicted line
-    (*cache)->Insert(*evicted);
-    // Repeat if necessary...
-    evicted = tmp;
-    cache++;
-  }
-
-  // If we are not holding any evicted line and have not found the requested line,
-  // continue searching.
-  while (cache < caches.end() && requested == NULL && evicted == NULL) {
-    if ((*cache)->Contains(address)) {
-      requested = (*cache)->GetLine(address);
-      // Remove the line from the cache and move it to L1 (exclusive cache).
-      (*cache)->RemoveLine(address);
-    }
-
-    cache++;
-  }
-
-  // We have reached the end of the cache hierarchy.
-  if (requested == NULL) {
-    // Line was not mapped in cache. Create it and insert it in L1.
-    requested = new CacheLine(line_size_B,
-        address - caches.front()->GetLineOffset(address));
-    caches.front()->Insert(*requested);
-  }
-
-  if (evicted != NULL) {
-    // We have evicted a line from the cache hierarchy. Delete it.
-    std::vector<bool> accessedBytes = evicted->getAccessedBytes();
-    int utilization = std::count(accessedBytes.begin(), accessedBytes.end(),
-        true);
-    byte_utilizations.at(utilization - 1)++;delete
-evicted    ;
-  }
-  return *requested;
 }
 
 CacheLine& MultilevelCache::SearchInclusive(const ADDRESS address,
@@ -243,8 +136,4 @@ CacheLine& MultilevelCache::SearchInclusive(const ADDRESS address,
     delete evicted;
   }
   return *requested;
-}
-
-void MultilevelCache::PrintSummary() {
-
 }
--- src/MultilevelCache.h
+++ src/MultilevelCache.h
@@ -20,10 +20,28 @@ private:
   std::vector<Cache*> caches;
 
 private:
+  /**
+   * Splits the access request if it spans multiple CacheLines.
+   * Returns a vector of accessed CacheLines.
+   */
   std::vector<CacheLine*>& SplitAccess(const ADDRESS address,
       const uint8_t n_bytes);
 
-  CacheLine& SearchExclusive(const ADDRESS address, const uint8_t size_B);
+  /**
+   * Searches the cache for the CacheLine containing the requested address.
+   *
+   * Algorithm:
+   * 1.  If the line is in the first level cache, fetch it and return.
+   * 2.  Else, if the first level cache is full, evict a line.
+   * 3.  Search the next level cache for the line. If we find it, remove it.
+   * 4.  If we previously evicted a line and the cache is full, evict a line.
+   * 4.1.Insert the previously evicted line into the cache.
+   * 5.  Repeat 3-4.1 until the end of the cache hierarchy.
+   * 6.  If we evicted a line from the LLC, destroy it.
+   * 7.  If we have not found the requested cache line, create it (ie. fetch from main memory).
+   * 8.  Insert the requested cache line into the L1.
+   *
+   */
   CacheLine& SearchInclusive(const ADDRESS address, const uint8_t size_B);
 
 public:
@@ -32,14 +50,26 @@ public:
   const uint32_t line_size_B;
 
 public:
+  /**
+   * Constructs a multilevel cache.
+   *
+   * First level cache must be specified in the first entry of vector, second
+   * in second entry and so on.
+   *
+   * @param capacities_B Cache capacities for each level of the cache, in bytes.
+   * @param associativities Cache associativities for each level of the cache.
+   * @param line_size_B The number of bytes each cache line will hold, defaults to 64.
+   */
   MultilevelCache(const std::vector<uint32_t>& capacities_B,
       const std::vector<uint32_t>& assocativities, const uint32_t line_size_B =
       DEFAULT_LINE_SIZE);
   virtual ~MultilevelCache();
 
+  /**
+   * Access the cache for a load or store operation.
+   * Returns a vector of CacheLines that contain the address requested.
+   */
   std::vector<CacheLine*>& Access(const ADDRESS address, const uint8_t n_bytes);
-
-  void PrintSummary();
 };
 
 #endif /* MULTILEVELCACHE_H_ */
